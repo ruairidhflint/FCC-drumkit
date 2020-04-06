1ee0b160da94c6e71ab47bf46d2adf4319ea6732
Rory Flint <roryflintphoto@gmail.com>
2020-04-06 15:24:49 +0100

    basic audio functionality working

--- src/Components/DrumContainer.js
+++ src/Components/DrumContainer.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react';
-
 import DrumPad from './DrumPad';
+
+import Clap from '../Sounds/Clap.wav';
+import Crash from '../Sounds/Crash.wav';
+import Perc01 from '../Sounds/Perc01.wav';
+import Perc02 from '../Sounds/Perc02.wav';
+import ClHat from '../Sounds/ClHat.wav';
+import Snare01 from '../Sounds/Snare01.wav';
+import Snare02 from '../Sounds/Snare02.wav';
+import Tom01 from '../Sounds/Tom01.wav';
+import Tom02 from '../Sounds/Tom02.wav';
 
 function DrumContainer() {
   const [keyPressed, setKeyPressed] = useState(null);
@@ -16,19 +25,64 @@ function DrumContainer() {
   return (
     <div id="drum-machine">
       <div className="row">
-        <DrumPad letter="Q" keyPressed={keyPressed} />
-        <DrumPad letter="W" keyPressed={keyPressed} />
-        <DrumPad letter="E" keyPressed={keyPressed} />
+        <DrumPad
+          letter="Q"
+          keyPressed={keyPressed}
+          setKeyPressed={setKeyPressed}
+          sound={Clap}
+        />
+        <DrumPad
+          letter="W"
+          keyPressed={keyPressed}
+          setKeyPressed={setKeyPressed}
+          sound={Crash}
+        />
+        <DrumPad
+          letter="E"
+          keyPressed={keyPressed}
+          setKeyPressed={setKeyPressed}
+          sound={Perc01}
+        />
       </div>
       <div className="row">
-        <DrumPad letter="A" keyPressed={keyPressed} />
-        <DrumPad letter="S" keyPressed={keyPressed} />
-        <DrumPad letter="D" keyPressed={keyPressed} />
+        <DrumPad
+          letter="A"
+          keyPressed={keyPressed}
+          setKeyPressed={setKeyPressed}
+          sound={Perc02}
+        />
+        <DrumPad
+          letter="S"
+          keyPressed={keyPressed}
+          setKeyPressed={setKeyPressed}
+          sound={ClHat}
+        />
+        <DrumPad
+          letter="D"
+          keyPressed={keyPressed}
+          setKeyPressed={setKeyPressed}
+          sound={Snare01}
+        />
       </div>
       <div className="row">
-        <DrumPad letter="Z" keyPressed={keyPressed} />
-        <DrumPad letter="X" keyPressed={keyPressed} />
-        <DrumPad letter="C" keyPressed={keyPressed} />
+        <DrumPad
+          letter="Z"
+          keyPressed={keyPressed}
+          setKeyPressed={setKeyPressed}
+          sound={Snare02}
+        />
+        <DrumPad
+          letter="X"
+          keyPressed={keyPressed}
+          setKeyPressed={setKeyPressed}
+          sound={Tom01}
+        />
+        <DrumPad
+          letter="C"
+          keyPressed={keyPressed}
+          setKeyPressed={setKeyPressed}
+          sound={Tom02}
+        />
       </div>
     </div>
   );
--- src/Components/DrumPad.js
+++ src/Components/DrumPad.js
@@ -3,6 +3,10 @@ import React from 'react';
 function DrumPad(props) {
   const click = () => {
     console.log(props.letter);
+    const audio = document.getElementById(`audio-${props.letter}`);
+    audio.currentTime = 0;
+    audio.play();
+    props.setKeyPressed(null);
   };
 
   if (props.keyPressed === props.letter) {
@@ -11,6 +15,7 @@ function DrumPad(props) {
 
   return (
     <div onClick={click} className="drum-pad" id={props.letter}>
+      <audio id={`audio-${props.letter}`} src={props.sound} />
       {props.letter}
     </div>
   );

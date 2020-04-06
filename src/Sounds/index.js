8703b7bb1a0a279962f03b07fdd2a9f2b5404373
Rory Flint <roryflintphoto@gmail.com>
2020-04-06 19:22:39 +0100

    Add index file for sounds

---
+++ src/Sounds/index.js
@@ -0,0 +1,54 @@
+import Bass from './606bass.wav';
+import Clap from './808clap.wav';
+import Cowbell from './808cowbell.wav';
+import HiHat from './808hihat2.wav';
+import Kick from './808kick.wav';
+import Maraca from './808maraca.wav';
+import OpenHat from './808op_hat.wav';
+import Snare from './808snare.wav';
+import Punch from './Bt7a0da.wav';
+
+export const bass = {
+  sound: Bass,
+  name: '606 Bass',
+};
+
+export const clap = {
+  sound: Clap,
+  name: '808 Clap',
+};
+
+export const cowbell = {
+  sound: Cowbell,
+  name: '808 Cowbell',
+};
+
+export const hihat = {
+  sound: HiHat,
+  name: '808 Hi-hat',
+};
+
+export const kick = {
+  sound: Kick,
+  name: '808 Kick',
+};
+
+export const maraca = {
+  sound: Maraca,
+  name: '808 Maraca',
+};
+
+export const openHat = {
+  sound: OpenHat,
+  name: '808 Open-hat',
+};
+
+export const snare = {
+  sound: Snare,
+  name: '808 Snare',
+};
+
+export const punch = {
+  sound: Punch,
+  name: '808 Punch',
+};
